776a68ad846edf180023be1dfcf78a9bcdafdc3e
OurdiaD <daideche.o@gmail.com>
2021-05-03 14:30:35 +0200

    tests passed

--- app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
+++ app/src/main/java/com/openclassrooms/magicgithub/api/FakeApiService.java
@@ -15,7 +15,7 @@ public class FakeApiService implements ApiService {
     @Override
     public List<User> getUsers() {
         // TODO: A modifier
-        return null;
+        return users;
     }
 
     /**
@@ -25,6 +25,8 @@ public class FakeApiService implements ApiService {
     @Override
     public void generateRandomUser() {
         // TODO: A modifier
+        User user = User.random();
+        users.add(user);
     }
 
     /**
@@ -33,5 +35,6 @@ public class FakeApiService implements ApiService {
     @Override
     public void deleteUser(User user) {
         // TODO: A modifier
+        users.remove(user);
     }
 }
